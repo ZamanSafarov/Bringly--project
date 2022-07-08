3e3cf7e37af0fd2d99f6a84fc265b2f61c914d7a
Ali Alizade <aalizade@aris.az>
2022-07-08 17:35:53 +0400

    end home page

--- assets/javascripts/used/5main.js
+++ assets/javascripts/used/5main.js
@@ -1,5 +1,18 @@
 $(document).ready(function () {
     console.log('asas');
+
+    $('.js--openMenu').on('click',function () {
+        $('.mobileMenu').addClass('open');
+        $('html, body').css('overflowY', 'hidden');
+
+    })
+    $('.js--closeMenu').on('click',function () {
+        $('.mobileMenu').removeClass('open');
+        $('html, body').css('overflowY', 'auto');
+    })
+
+    var aboutRightImdHeight = $('.about__right  > img').height();
+    $('.about__right--layout').height(aboutRightImdHeight);
     $(".js--riversSlider").slick({
         dots: true,
         infinite: false,
@@ -8,9 +21,6 @@ $(document).ready(function () {
         slidesToShow: 1,
         slidesToScroll: 1
 
-        // appendArrows: '.projects__slider-arrows-h',
-        // prevArrow: $('.category--arrow-left-js'),
-        // nextArrow: $('.category--arrow-right-js'),
         // responsive: [
         //     {
         //         breakpoint: 1024,
@@ -45,35 +55,39 @@ $(document).ready(function () {
         slidesToShow: 4,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 1000
+        autoplaySpeed: 1000,
 
         // appendArrows: '.projects__slider-arrows-h',
         // prevArrow: $('.category--arrow-left-js'),
         // nextArrow: $('.category--arrow-right-js'),
-        // responsive: [
-        //     {
-        //         breakpoint: 1024,
-        //         settings: {
-        //             slidesToShow: 1,
-        //             slidesToScroll: 1,
-        //             infinite: true,
-        //             dots: true
-        //         }
-        //     },
-        //     {
-        //         breakpoint: 768,
-        //         settings: {
-        //             slidesToShow: 2,
-        //             slidesToScroll: 2
-        //         }
-        //     },
-        //     {
-        //         breakpoint: 570,
-        //         settings: {
-        //             slidesToShow: 1,
-        //             slidesToScroll: 1
-        //         }
-        //     }
-        // ]
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 570,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            }
+        ]
     });
 });
