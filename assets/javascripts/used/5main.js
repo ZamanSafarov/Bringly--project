319b714bc89619e9fbb7d62a9f2cc267bc61c543
Ali Alizade <aalizade@aris.az>
2022-07-15 03:54:27 +0400

    [edit] home page

--- assets/javascripts/used/5main.js
+++ assets/javascripts/used/5main.js
@@ -1,5 +1,20 @@
 $(document).ready(function () {
-    console.log('asas');
+
+    $("#bgndVideo").YTPlayer();
+
+    $('.js--playVideo').on('click',function () {
+        $('#bgndVideo').YTPPlay();
+        $('.js--playVideo').css('display', 'none');
+        $('.js--pouseVideo').css('display', 'block');
+    })
+    $('.js--pouseVideo').on('click',function () {
+        $('#bgndVideo').YTPPause();
+        $('.js--pouseVideo').css('display', 'none');
+        $('.js--playVideo').css('display', 'block');
+    })
+
+
+
 
     $('.js--openMenu').on('click',function () {
         $('.mobileMenu').addClass('open');
@@ -11,6 +26,7 @@ $(document).ready(function () {
         $('html, body').css('overflowY', 'auto');
     })
 
+
     var aboutRightImdHeight = $('.about__right  > img').height();
     $('.about__right--layout').height(aboutRightImdHeight);
     $(".js--riversSlider").slick({
@@ -20,32 +36,6 @@ $(document).ready(function () {
         fade: false,
         slidesToShow: 1,
         slidesToScroll: 1
-
-        // responsive: [
-        //     {
-        //         breakpoint: 1024,
-        //         settings: {
-        //             slidesToShow: 1,
-        //             slidesToScroll: 1,
-        //             infinite: true,
-        //             dots: true
-        //         }
-        //     },
-        //     {
-        //         breakpoint: 768,
-        //         settings: {
-        //             slidesToShow: 2,
-        //             slidesToScroll: 2
-        //         }
-        //     },
-        //     {
-        //         breakpoint: 570,
-        //         settings: {
-        //             slidesToShow: 1,
-        //             slidesToScroll: 1
-        //         }
-        //     }
-        // ]
     });
     $(".js--partnyorsSlider").slick({
         dots: false,
@@ -56,10 +46,6 @@ $(document).ready(function () {
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 1000,
-
-        // appendArrows: '.projects__slider-arrows-h',
-        // prevArrow: $('.category--arrow-left-js'),
-        // nextArrow: $('.category--arrow-right-js'),
         responsive: [
             {
                 breakpoint: 1024,
@@ -90,4 +76,23 @@ $(document).ready(function () {
             }
         ]
     });
+});
+$(document).on('click', 'a[href^="#"]', function(e) {
+    // target element id
+    var id = $(this).attr('href');
+
+    // target element
+    var $id = $(id);
+    if ($id.length === 0) {
+        return;
+    }
+
+    // prevent standard hash navigation (avoid blinking in IE)
+    e.preventDefault();
+
+    // top position relative to the document
+    var pos = $id.offset().top;
+
+    // animated top scrolling
+    $('body, html').animate({scrollTop: pos},1000);
 });
